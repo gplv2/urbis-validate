b5d80577f26e898528548adbc754769a2b98ed79
Glenn Plas <glenn@byte-consult.be>
2016-10-17 10:49:17 +0200

    update exception list

--- special.php
+++ special.php
@@ -1,5 +1,13 @@
 <?php
 $special_streets = array(
-'Avenue de la Chevalerie - Ridderschaplaan',
-'Avenue de la Chevalerie - Ridderschapslaan',
+"Avenue de la Chevalerie - Ridderschaplaan",
+"Avenue de la Chevalerie - Ridderschapslaan",
+"Rue de la Grosse Tour - Wollendriestoren",
+"Rue de la Grosse Tour - Wollendriestorenstraat",
+"Rue de la Cambre - Terkamerenstraat",
+"Rue de la Cambre - Ter Kamerenstraat",
+"Chaussée de Louvain - Leuvensesteenweg",
+"Chaussée de Louvain - Leuvense Steenweg",
+"Avenue du Val d'Or - Guldendallaan",
+"Avenue du Val d'Or - Gouddallaan"
 );
